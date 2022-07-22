946031d0973faba6094b6f3478cc105a6d7e67ab
Mahendar Mukthavaram <mm22228@corp.ojas-it.com>
2022-07-22 10:30:33 +0530

    adde encapsulation

---
+++ src/com/encap/Student.java
@@ -0,0 +1,15 @@
+package com.encap;
+
+public class Student {
+	
+private String name;// private data member
+
+public String getName() //getter method
+{
+	return name;
+}
+ public void setName(String name)// setter method
+ {
+	 this.name=name;
+ }
+}
---
+++ src/com/encap/Test.java
@@ -0,0 +1,12 @@
+package com.encap;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student s = new Student();
+s.setName("Mahee");
+System.out.println(s.getName());
+	}
+
+}
